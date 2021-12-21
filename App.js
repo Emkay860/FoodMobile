b5d074991e07172e7ecd0e53d31f7a1a76e0e379
Majid Kareem <kareemmajid86@gmail.com>
2021-12-21 13:51:45 +0100

    Add new components for vendors

--- App.js
+++ App.js
@@ -6,8 +6,9 @@ import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import SignIn from './src/screens/SignIn';
 import Register from './src/screens/Register';
-import Home from './src/screens/Home';
 import BottomNavBar from './src/screens/components/BottomNavBar';
+import MenuDetail from './src/screens/components/MenuDetail';
+import CreateMenuItem from './src/screens/components/CreateMenuItem';
 
 const Stack = createStackNavigator();
 
@@ -15,7 +16,7 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="Create-Menu-Item">
           <Stack.Screen
             name="SplashScreen"
             component={SplashScreen}
@@ -36,6 +37,16 @@ export default function App() {
           <Stack.Screen
             name="Home"
             component={BottomNavBar}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Menu-Detail"
+            component={MenuDetail}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Create-Menu-Item"
+            component={CreateMenuItem}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
---
+++ src/screens/components/CreateMenuItem.js
@@ -0,0 +1,142 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import {
+  Button,
+  Appbar,
+  TextInput,
+  Headline,
+  Caption,
+  Paragraph,
+} from 'react-native-paper';
+function CreateMenuItem() {
+  return (
+    <>
+      <SafeAreaView>
+        <Appbar.Header style={styles.header}>
+          <Appbar.BackAction onPress={() => navigation.push('SplashScreen')} />
+          {/* <Appbar.Content
+            title="Sign in"
+            color="black"
+            titleStyle={[styles.appBarTitle, styles.active]}
+            style={styles.appBarNav}
+          />
+          <Appbar.Content
+            title="Register"
+            color="black"
+            titleStyle={styles.appBarTitle}
+            style={styles.appBarNav}
+            onPress={() => navigation.navigate('Register')}
+          /> */}
+        </Appbar.Header>
+      </SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.textContainer}>
+          <Headline style={styles.headingText}>Create New Item</Headline>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            mode="outlined"
+            placeholder="Title"
+            theme={{
+              colors: { primary: '#FA9B0B', underlineColor: 'transparent' },
+              roundness: 8,
+            }}
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            mode="outlined"
+            placeholder="Price"
+            theme={{
+              colors: { primary: '#FA9B0B', underlineColor: 'transparent' },
+              roundness: 8,
+            }}
+            style={styles.input}
+            textContentType="password"
+            secureTextEntry={true}
+          />
+        </View>
+
+        <Button
+          mode="contained"
+          onPress={() => navigation.push('Home')}
+          color="#FA9B0B"
+          icon="plus"
+          contentStyle={styles.signInBtnContent}
+          style={styles.signInBtn}
+          uppercase={false}
+          labelStyle={styles.signInBtnLabel}
+        >
+          Add Item
+        </Button>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: '#F8F8F8',
+    paddingTop: 100,
+  },
+  header: {
+    backgroundColor: '#FFFFFF',
+  },
+  appBarNav: {
+    alignItems: 'center',
+  },
+  appBarTitle: {
+    padding: 5,
+    fontSize: 16,
+  },
+  active: {
+    borderBottomWidth: 3,
+    borderBottomColor: '#FA9B0B',
+  },
+  captionText: {
+    fontSize: 16,
+  },
+  forgotPasswordContainer: {
+    alignItems: 'flex-end',
+    width: '80%',
+    paddingTop: 10,
+  },
+  forgotPasswordText: {
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+  input: {
+    backgroundColor: '#FFFFFF',
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  textContainer: {
+    paddingBottom: 40,
+    width: '80%',
+  },
+  headingText: {
+    fontSize: 32,
+    color: 'black',
+  },
+  signInBtnContent: {
+    flexDirection: 'row-reverse',
+    justifyContent: 'space-between',
+  },
+  signInBtn: {
+    width: '80%',
+    padding: 5,
+    borderRadius: 8,
+  },
+  signInBtnLabel: {
+    fontSize: 16,
+  },
+});
+
+export default CreateMenuItem;
